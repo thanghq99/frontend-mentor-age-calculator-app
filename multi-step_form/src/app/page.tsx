3e95bd3925c2ef03d56084d66164136718c6d54b
thanghq99 <haikhauak@gmail.com>
2023-07-20 13:55:40 +0700

    feat: init app

---
+++ multi-step_form/src/app/page.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+
+const Home: FC = () => {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-center bg-[#EEF4FB]'>
+      <div className='w-[375px] p-2 flex flex-col'>
+        <div>form step selector</div>
+        <div>
+          <div>form</div>
+          <div>form controller</div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
