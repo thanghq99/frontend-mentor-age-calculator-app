3a526c897d0069ac1953cb87a41db1d386bf7a56
thanghq99 <haikhauak@gmail.com>
2023-07-24 11:57:40 +0700

    init: init project

---
+++ space_tourism/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className='min-h-screen font-barlow'>
+      page with barlow as default font
+    </main>
+  );
+}
---
+++ space_tourism/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'bellefair': ['var(--next-bellefair)'],
+        'barlow': ['var(--next-barlow)'],
+        'barlow-condensed': ['var(--next-barlow-condensed)'],
+      },
+    },
+  },
+  plugins: [],
+};
