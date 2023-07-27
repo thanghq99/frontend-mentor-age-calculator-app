071746bdf94aa024cb232bcafb51a2f3bba6eb2a
thanghq99 <haikhauak@gmail.com>
2023-07-27 16:20:33 +0700

    init: init project

---
+++ rest-country-theme-switch/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <form action=''>
+        <input type='text' name='test' />
+        <button type='submit'>submit</button>
+      </form>
+    </main>
+  );
+}
