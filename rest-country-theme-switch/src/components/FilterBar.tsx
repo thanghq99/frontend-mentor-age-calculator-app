1295b84e24d3634c2a817877d3fa94e394cedf6a
thanghq99 <haikhauak@gmail.com>
2023-07-31 11:34:13 +0700

    fix: fix filter bar

--- rest-country-theme-switch/src/components/FilterBar.tsx
+++ rest-country-theme-switch/src/components/FilterBar.tsx
@@ -2,14 +2,7 @@
 
 import React, { useState } from 'react';
 
-const regions = [
-  'Filter by Region',
-  'Africa',
-  'America',
-  'Asia',
-  'Europe',
-  'Oceania',
-] as const;
+const regions = ['', 'Africa', 'America', 'Asia', 'Europe', 'Oceania'] as const;
 
 export type Region = (typeof regions)[number];
 
@@ -21,17 +14,33 @@ const FilterBar = () => {
   };
 
   return (
-    <select
-      value={selectedRegion}
-      onChange={selectRegion}
-      className='w-1/2 lg:w-1/4 p-4 focus:outline-none text-l-text dark:text-d-text bg-l-element dark:bg-d-element shadow-md'
-    >
-      {regions.map((region, key) => (
-        <option key={key} value={region} selected={region === selectedRegion}>
-          {region}
-        </option>
-      ))}
-    </select>
+    <div className='relative w-1/2 lg:w-1/4'>
+      <select
+        value={selectedRegion}
+        onChange={selectRegion}
+        className='w-full p-4 rounded-md focus:outline-none text-l-text dark:text-d-text bg-l-element dark:bg-d-element shadow-md'
+      >
+        {regions.map((region, key) => (
+          <option key={key} value={region}>
+            {region === '' ? 'Filter by Region' : region}
+          </option>
+        ))}
+      </select>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        strokeWidth={2}
+        stroke='currentColor'
+        className='absolute w-4 h-4 bottom-5 right-4 pointer-events-none'
+      >
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M19.5 8.25l-7.5 7.5-7.5-7.5'
+        />
+      </svg>
+    </div>
   );
 };
 
