29140413575dafa38d54c7d5ead9e6632187832b
thanghq99 <haikhauak@gmail.com>
2023-07-31 11:20:01 +0700

    feat: add country card

--- rest-country-theme-switch/src/app/page.tsx
+++ rest-country-theme-switch/src/app/page.tsx
@@ -2,13 +2,19 @@ import FilterBar from '@/components/FilterBar';
 import SearchBar from '@/components/SearchBar';
 import { ComponentProps } from '@/types/routeType';
 import { FC } from 'react';
+import countries from '@/data/data.json';
+import CountryCard from '@/components/CountryCard';
+
+export type Country = (typeof countries)[number];
 
 const Home: FC<ComponentProps> = () => {
   return (
     <div className='flex flex-col space-y-8 my-6 px-4'>
       <SearchBar />
       <FilterBar />
-      <div>home</div>
+      {countries.map((country, key) => (
+        <CountryCard country={country} key={key} />
+      ))}
     </div>
   );
 };
---
+++ rest-country-theme-switch/src/components/CountryCard.tsx
@@ -0,0 +1,33 @@
+import { Country } from '@/app/page';
+import React, { FC } from 'react';
+
+const CountryCard: FC<{ country: Country }> = ({ country }) => {
+  return (
+    <div className='flex flex-col mx-8 bg-l-element dark:bg-d-element rounded-md shadow-md'>
+      <img
+        src={country.flag}
+        alt={`${country.name}-flag`}
+        className='w-full h-auto'
+      />
+      <div className='p-6'>
+        <p className='font-extrabold text-lg mb-6'>{country.name}</p>
+        <div className='flex flex-col space-y-1'>
+          <p>
+            <span className='font-bold'>Population: </span>
+            {country.population.toLocaleString()}
+          </p>
+          <p>
+            <span className='font-bold'>Region: </span>
+            {country.region}
+          </p>
+          <p>
+            <span className='font-bold'>Capital: </span>
+            {country.capital}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountryCard;
