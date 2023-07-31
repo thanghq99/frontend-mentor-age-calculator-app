1297b8e76b28b0622846dcc960634761da124404
thanghq99 <haikhauak@gmail.com>
2023-07-31 11:35:46 +0700

    fix: fix header

--- rest-country-theme-switch/src/components/Header.tsx
+++ rest-country-theme-switch/src/components/Header.tsx
@@ -4,7 +4,7 @@ import ThemeButton from './ThemeButton';
 const Header: FC = () => {
   return (
     <div className='flex justify-between px-4 py-8 text-sm shadow-md dark:bg-d-element'>
-      <div className='font-extrabold'>Where in the world?</div>
+      <div className='text-base font-extrabold'>Where in the world?</div>
       <ThemeButton />
     </div>
   );
